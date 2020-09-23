8953d2fd383d0347818886b0a475fa2f724dfea1
Raynelle Francisca <raynelle.alphonso@theappbusiness.com>
2020-09-23 12:07:45 +0100

    Setup React Env

---
+++ scrapbook-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
